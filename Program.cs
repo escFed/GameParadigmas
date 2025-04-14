2e5afb37e86d3deef68105cbd46922bea8067322
escFed <escaladafederico1@gmail.com>
2025-04-13 21:20:44 -0300

    V 0.01
    Version base del proyecto

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using Tao.Sdl;
+
+
+
+namespace MyGame
+{
+
+    class Program
+    {
+        static Image fondo = Engine.LoadImage("assets/fondo.png");
+        static Image player = Engine.LoadImage("assets/player.png");
+
+
+        
+        static void Main(string[] args)
+        {
+            Engine.Initialize();
+
+            while (true)
+            {
+                Input();
+                Update();
+                Render();
+            }
+        }
+
+        static void Input()
+        {
+
+        }
+
+        static void Update()
+        {
+   
+
+        }
+
+        static void Render()
+        {
+            Engine.Clear();
+            Engine.Draw(fondo, 0, 0);
+            Engine.Draw(player, posX, 0);
+            Engine.Show();
+            
+        }
+    }
+}
